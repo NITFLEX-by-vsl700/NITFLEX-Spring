1f64b95b16f35119fe4983645e0b6f7d6036536e
vsl700 <51147745+vsl700@users.noreply.github.com>
2023-11-17 00:44:37 +0200

    Added more method description in WebClientService

--- src/main/java/com/vsl700/nitflex/services/WebClientService.java
+++ src/main/java/com/vsl700/nitflex/services/WebClientService.java
@@ -3,6 +3,14 @@ package com.vsl700.nitflex.services;
 import com.vsl700.nitflex.components.WebsiteCredentials;
 
 public interface WebClientService {
+    /**
+     * Performs a post request to the specified url and returns the cookies as a single String
+     * @param url the url to perform the request to (e.g. "https://zamunda.net/takelogin.php")
+     * @param usernameAttr the name of the query attribute that contains the username
+     * @param passwordAttr the name of the query attribute that contains the password
+     * @param websiteCredentials the credentials (username and password)
+     * @return
+     */
     String loginAndGetCookie(String url, String usernameAttr, String passwordAttr, WebsiteCredentials websiteCredentials);
 
     /**
