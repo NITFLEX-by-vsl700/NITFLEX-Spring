aae45b8f973594c4bfe113692dca1862bcbfb4b2
vsl700 <51147745+vsl700@users.noreply.github.com>
2023-11-28 20:33:09 +0200

    Forgot to annotate WebClientServiceImpl as @Service

--- src/main/java/com/vsl700/nitflex/services/implementations/WebClientServiceImpl.java
+++ src/main/java/com/vsl700/nitflex/services/implementations/WebClientServiceImpl.java
@@ -5,6 +5,7 @@ import com.vsl700.nitflex.services.WebClientService;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -15,6 +16,7 @@ import reactor.netty.http.client.HttpClient;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+@Service
 public class WebClientServiceImpl implements WebClientService {
     @Override
     public String loginAndGetCookie(String url, String usernameAttr, String passwordAttr, WebsiteCredentials websiteCredentials) {
