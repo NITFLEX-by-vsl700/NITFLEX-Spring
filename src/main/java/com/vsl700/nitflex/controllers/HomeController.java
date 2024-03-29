3e5f69b2563fb2cc18a860078eca9087956256d8
vsl700 <51147745+vsl700@users.noreply.github.com>
2024-03-29 13:14:33 +0200

    Finished Movies API logic

--- src/main/java/com/vsl700/nitflex/controllers/HomeController.java
+++ src/main/java/com/vsl700/nitflex/controllers/HomeController.java
@@ -9,6 +9,7 @@ import com.vsl700.nitflex.models.dto.SubtitleDTO;
 import com.vsl700.nitflex.repo.EpisodeRepository;
 import com.vsl700.nitflex.repo.MovieRepository;
 import com.vsl700.nitflex.repo.SubtitleRepository;
+import jdk.jshell.spi.ExecutionControl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -43,52 +44,62 @@ public class HomeController {
 
     @GetMapping("/movies/{movieId}")
     public MovieDTO getMovieById(@PathVariable String movieId){
+        if(movieRepository.findById(movieId).isEmpty())
+            throw new RuntimeException("Movie with id %s not found!".formatted(movieId)); // TODO Add custom exception
+
         return modelMapper.map(movieRepository.findById(movieId), MovieDTO.class);
     }
 
     @GetMapping("/episodes/{movieId}")
     public List<EpisodeDTO> getEpisodesByMovieId(@PathVariable String movieId){
+        if(movieRepository.findById(movieId).isEmpty())
+            throw new RuntimeException("Movie with id %s not found!".formatted(movieId)); // TODO Add custom exception
+
         return episodeRepository.findAllBySeriesId(movieId).stream()
                 .map(e -> modelMapper.map(e, EpisodeDTO.class))
                 .toList();
     }
 
     @GetMapping("/subtitles/{movieId}")
-    public List<SubtitleDTO> getSubtitlesByMovieId(@PathVariable String movieId){
+    public List<SubtitleDTO> getAllSubtitlesByMovieId(@PathVariable String movieId){
+        if(movieRepository.findById(movieId).isEmpty())
+            throw new RuntimeException("Movie with id %s not found!".formatted(movieId)); // TODO Add custom exception
+
         return subtitleRepository.findAllByMovieId(movieId).stream()
                 .map(s -> modelMapper.map(s, SubtitleDTO.class))
                 .toList();
     }
 
-    @GetMapping("/subtitles/{movieId}/{episodeId}")
-    public List<SubtitleDTO> getSeriesSubtitlesByMovieId(@PathVariable String movieId, @PathVariable String episodeId){
-        Episode episode = episodeRepository.findById(episodeId)
-                .orElseThrow(); // TODO Add custom exception
+    @GetMapping("/subtitles/{movieId}/trailer")
+    public List<SubtitleDTO> getTrailerSubtitlesByMovieId(@PathVariable String movieId){
+        if(movieRepository.findById(movieId).isEmpty())
+            throw new RuntimeException("Movie with id %s not found!".formatted(movieId)); // TODO Add custom exception
 
-        List<SubtitleDTO> result = subtitleRepository.findAllByMovieId(movieId).stream()
-                .filter(s -> areSubtitlesForGivenEpisode(s, episode))
+        return subtitleRepository.findAllByMovieId(movieId).stream()
+                .filter(s -> s.getType().equals(Subtitle.SubtitleType.Trailer))
                 .map(s -> modelMapper.map(s, SubtitleDTO.class))
                 .toList();
-
-        if(result.isEmpty())
-            return subtitleRepository.findAllByMovieId(movieId).stream()
-                    .map(s -> modelMapper.map(s, SubtitleDTO.class))
-                    .toList();
-
-        return result;
     }
 
-    private boolean areSubtitlesForGivenEpisode(Subtitle subtitle, Episode episode){
-        Path subtitlePath = Path.of(subtitle.getPath());
-        Path episodePath = Path.of(episode.getEpisodePath());
+    @GetMapping("/subtitles/{movieId}/film")
+    public List<SubtitleDTO> getFilmSubtitlesByMovieId(@PathVariable String movieId){
+        if(movieRepository.findById(movieId).isEmpty())
+            throw new RuntimeException("Movie with id %s not found!".formatted(movieId)); // TODO Add custom exception
 
-        if(subtitlePath.getNameCount() > 1) {
-            Path subtitleParentFolder = subtitlePath.subpath(0, subtitlePath.getNameCount() - 1);
-            if (subtitleParentFolder.toString().equals(episodePath.toString()))
-                return true;
-        }
+        return subtitleRepository.findAllByMovieId(movieId).stream()
+                .filter(s -> s.getType().equals(Subtitle.SubtitleType.Film) || s.getType().equals(Subtitle.SubtitleType.Undetermined))
+                .map(s -> modelMapper.map(s, SubtitleDTO.class))
+                .toList();
+    }
 
-        return subtitle.getPath().contains("S0%dE0%d".formatted(episode.getSeasonNumber(), episode.getEpisodeNumber()))
-                || subtitle.getPath().contains("S%dE%d".formatted(episode.getSeasonNumber(), episode.getEpisodeNumber()));
+    @GetMapping("/subtitles/{movieId}/episode/{episodeId}")
+    public List<SubtitleDTO> getSeriesSubtitlesByMovieId(@PathVariable String movieId, @PathVariable String episodeId){
+        if(episodeRepository.findAllBySeriesId(movieId).stream().noneMatch(e -> e.getId().equals(episodeId)))
+            throw new RuntimeException("Episode %s not found in movie %s!".formatted(episodeId, movieId)); // TODO Add custom exception
+
+        return subtitleRepository.findAllByMovieId(movieId).stream()
+                .filter(s -> episodeId.equals(s.getEpisodeId()) || s.getType().equals(Subtitle.SubtitleType.Undetermined))
+                .map(s -> modelMapper.map(s, SubtitleDTO.class))
+                .toList();
     }
 }
