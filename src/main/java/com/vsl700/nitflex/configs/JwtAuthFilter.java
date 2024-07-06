27069a4aee9b3901dc1679e4ce8a893763f79f9b
vsl700 <51147745+vsl700@users.noreply.github.com>
2024-07-06 14:09:42 +0300

    Update JwtAuthFilter.java

--- src/main/java/com/vsl700/nitflex/configs/JwtAuthFilter.java
+++ src/main/java/com/vsl700/nitflex/configs/JwtAuthFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
